67c4e218d326efecca62b232b4d0f38e358b3359
unknown <aprajput331998@gmail.com>
2025-09-10 16:51:11 +0530

    fix:taking x-tenant header

--- middleware/tenantMiddleware.js
+++ middleware/tenantMiddleware.js
@@ -3,8 +3,10 @@ import { getTenantConnection } from "../utils/tenantManager.js";
 
 export async function tenantMiddleware(req, res, next) {
   try {
-    const host = req.headers.host; // e.g. hr.myvms.com
-    const subdomain = host.split(".")[0]; // "hr"
+    // const host = req.headers.host; // e.g. hr.myvms.com
+    // const subdomain = host.split(".")[0]; // "hr"
+
+    const subdomain = req.headers["x-tenant"];
 
     if (!subdomain) {
       return res.status(400).json({ error: "No subdomain found" });
