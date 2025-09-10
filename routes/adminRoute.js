8fd67c25319d07df374410b9097d1c85069e8066
prathmeshpote99 <poteprathmesh@gmail.com>
2025-09-10 18:08:38 +0530

    feat: Done Admin CRUD cycle

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,9 +1,18 @@
-import { createAdmin, getAdmin } from "../controllers/adminController.js";
+import {
+  createAdmin,
+  getAdmin,
+  getAdminById,
+  updateAdmin,
+  deleteAdmin,
+} from "../controllers/adminController.js";
 import express from "express";
 
 const router = express();
 
 router.post("/createAdmin", createAdmin);
 router.get("/getAdmin", getAdmin);
+router.get("/getAdminById/:id", getAdminById);
+router.patch("/updateAdmin/:id", updateAdmin);
+router.delete("/deleteAdmin/:id", deleteAdmin);
 
 export default router;
